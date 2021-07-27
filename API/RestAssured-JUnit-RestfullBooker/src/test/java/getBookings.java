8de7e8faa83e5be7c528b031f293968f829a43bf
azeljkovic <aleksandar.zeljkovic91@gmail.com>
2021-07-27 22:25:50 +0200

    added initial getBookings test

--- API/RestAssured-JUnit-RestfullBooker/src/test/java/getBookings.java
+++ API/RestAssured-JUnit-RestfullBooker/src/test/java/getBookings.java
@@ -1,13 +1,15 @@
 import helpers.parseJSON;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class getBookings {
     public static String BASE_URL = parseJSON.getValue("baseURL");
     public static String GET_BOOKINGS = parseJSON.getValue("getBookings");
+
+    public static String FIRST_NAME = parseJSON.getValue("firstname");
 
 
     @Test
@@ -17,10 +19,25 @@ public class getBookings {
             get(BASE_URL + GET_BOOKINGS).
         then().
             assertThat().
-            statusCode(200).contentType(ContentType.JSON).
+                statusCode(200).
+                contentType(ContentType.JSON).
             log().body();
+    }
 
+    @Test
+    public void getBookingByID() {
+        given().
+                // param("firstname", "Mark").
+        when().
+            get(BASE_URL + GET_BOOKINGS + "/1").
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("firstname", equalTo("Eric")).
+                 body(equalTo(parseJSON.getWholeBody())).
 
+            log().body();
     }
 
 }
--- API/RestAssured-JUnit-RestfullBooker/src/test/java/helpers/parseJSON.java
+++ API/RestAssured-JUnit-RestfullBooker/src/test/java/helpers/parseJSON.java
@@ -8,13 +8,13 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class parseJSON {
-    public static String JSON_PATH = "src/test/java/data/urls.json";
+    public static String URL_JSON = "src/test/java/data/urls.json";
 
     public static String getValue(String param) {
         String name = "";
         JSONParser parser = new JSONParser();
 
-        try (Reader reader = new FileReader(JSON_PATH)) {
+        try (Reader reader = new FileReader(URL_JSON)) {
 
             JSONObject jsonObject = (JSONObject) parser.parse(reader);
             name = (String) jsonObject.get(param);
@@ -23,5 +23,22 @@ public class parseJSON {
             e.printStackTrace();
         }
         return name;
+    }
+
+    public static String getWholeBody() {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = new JSONObject();
+
+        try (Reader reader = new FileReader("src/test/java/data/test.json")) {
+
+            jsonObject = (JSONObject) parser.parse(reader);
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        //return jsonObject;
+        //String jo=jsonObject.toString();
+        String jo = String.valueOf(jsonObject);
+        return jo;
     }
 }
