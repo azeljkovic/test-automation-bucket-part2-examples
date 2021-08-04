2905017cbf72ec5f322d0cf1908cf1026df785db
azeljkovic <aleksandar.zeljkovic91@gmail.com>
2021-08-04 21:43:35 +0200

    added getBookingIDs tests

---
+++ API/RestAssured-JUnit-RestfullBooker/src/test/java/data/urls.java
@@ -0,0 +1,6 @@
+package data;
+
+public class urls {
+    public static String base = "http://localhost:3001";
+    public static String getBookingIDs = "/booking";
+}
---
+++ API/RestAssured-JUnit-RestfullBooker/src/test/java/getBookingIDs.java
@@ -0,0 +1,156 @@
+import data.urls;
+import data.dataGetBookingIDs;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class getBookingIDs {
+    @Test
+    public void getAllBookingIDs() {
+        given().
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON);
+    }
+
+    // first name tests
+    @Test
+    public void getBookingIDByName() {
+        given().
+            param("firstname", dataGetBookingIDs.firstname).
+        when().
+                get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("bookingid[0]", equalTo(dataGetBookingIDs.bookingID));
+    }
+
+    @Test
+    public void getBookingIDByNonexistentName() {
+        given().
+            param("firstname", dataGetBookingIDs.nonexistentFirstname).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+            statusCode(200).
+            contentType(ContentType.JSON).
+            body("isEmpty()", is(true));
+    }
+
+    // last name tests
+    @Test
+    public void getBookingIDByLastName() {
+        given().
+            param("lastname", dataGetBookingIDs.lastname).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("bookingid[0]", equalTo(dataGetBookingIDs.bookingID));
+    }
+
+    @Test
+    public void getBookingIDByNonexistentLastName() {
+        given().
+            param("lastname", dataGetBookingIDs.nonexistentLastname).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("isEmpty()", is(true));
+    }
+
+    // checkin date tests
+    @Test
+    public void getBookingIDByCheckin() {
+        given().
+            param("checkin", dataGetBookingIDs.checkin).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("bookingid[0]", equalTo(dataGetBookingIDs.bookingID));
+    }
+
+    @Test
+    public void getBookingIDByNonexistentCheckin() {
+        given().
+            param("checkin", dataGetBookingIDs.nonexistentCheckinCheckout).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("isEmpty()", is(true));
+    }
+
+    @Test
+    public void getBookingIDByInvalidCheckin() {
+        given().
+            param("checkin", dataGetBookingIDs.invalidCheckinCheckout).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("isEmpty()", is(true));
+    }
+
+    // checkout date tests
+    @Test
+    public void getBookingIDByCheckout() {
+        given().
+            param("checkout", dataGetBookingIDs.checkout).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("bookingid[0]", equalTo(dataGetBookingIDs.bookingID));
+    }
+
+    @Test
+    public void getBookingIDByNonexistentCheckout() {
+        given().
+            param("checkout", dataGetBookingIDs.nonexistentCheckinCheckout).
+        when().
+            get(urls.base + urls.getBookingIDs).
+        then().
+            assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("isEmpty()", is(true));
+    }
+
+    @Test
+    public void getBookingIDByInvalidCheckout() {
+        given().
+                param("checkout", dataGetBookingIDs.invalidCheckinCheckout).
+                when().
+                get(urls.base + urls.getBookingIDs).
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("isEmpty()", is(true));
+    }
+
+}
