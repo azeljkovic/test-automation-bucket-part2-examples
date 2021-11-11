d7c445a429185041342fc4cbb12185cb204525c7
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2021-11-11 13:24:26 +0100

    added get id tests

---
+++ API/PactumJS-Mocha-RestfulBooker/test/getBooking.js
@@ -0,0 +1,33 @@
+const endpoints = require('../data/endpoints.json');
+const validResponseJSON = require('../data/validResponse.json');
+const responseData = require('../data/response.json');
+const pactum = require('pactum');
+const request = pactum.request;
+
+request.setBaseUrl('http://localhost:3001');
+
+it('Get specific valid ID', async () => {
+    await pactum.spec()
+        .get(endpoints.booking + endpoints.validID)
+        .withHeaders('Accept', 'application/json')
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJson(validResponseJSON);
+});
+
+it('Get specific valid ID without Accept header', async () => {
+    await pactum.spec()
+        .get(endpoints.booking + endpoints.validID)
+        .expectStatus(418)
+        .expectHeaderContains('content-type', 'text/plain')
+        .expectBody(responseData.emptyAcceptHeaderMessage);
+});
+
+it('Get specific invalid ID', async () => {
+    await pactum.spec()
+        .get(endpoints.booking + endpoints.invalidID)
+        .withHeaders('Accept', 'application/json')
+        .expectStatus(404)
+        .expectHeaderContains('content-type', 'text/plain')
+        .expectBody(responseData.invalidIDMessage);
+});
