8738394e5e38ab61a4e5a337c6d7349329a46130
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2021-11-11 10:19:35 +0100

    added get booking tests

--- API/PactumJS-Mocha-RestfulBooker/test/getBookingIDs.js
+++ API/PactumJS-Mocha-RestfulBooker/test/getBookingIDs.js
@@ -87,3 +87,75 @@ it('Get booking ID by existent checkin and checkout date', async () => {
             "bookingid": responseData.validBookingIDCheckinCheckoutDate
         }]);
 });
+
+it('Get booking ID by nonexistent firstname', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('firstname', requestData.nonExistentFirstname)
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
+
+it('Get booking ID by nonexistent lastname', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('lastname', requestData.nonExistentLastname)
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
+
+it('Get booking ID by nonexistent checkin date', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('checkin', requestData.nonExistentCheckinDate)
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
+
+it('Get booking ID by nonexistent checkout date', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('checkout', requestData.nonExistentCheckoutDate)
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
+
+it('Get booking ID - empty firstname header', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('firstname', "")
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
+
+it('Get booking ID - empty lastname header', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('lastname', "")
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
+
+it('Get booking ID - empty checkin date header', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('checkin', "")
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
+
+it('Get booking ID - empty checkout date header', async () => {
+    await pactum.spec()
+        .get(endpoints.booking)
+        .withQueryParams('checkout', "")
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonLength(0);
+});
