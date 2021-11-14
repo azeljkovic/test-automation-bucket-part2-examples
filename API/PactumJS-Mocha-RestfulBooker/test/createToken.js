bfa5f4f9da8c9cf2e8f5d9bba58889abe1c69c3c
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2021-11-14 19:51:37 +0100

    added auth tests

---
+++ API/PactumJS-Mocha-RestfulBooker/test/createToken.js
@@ -0,0 +1,49 @@
+const endpoints = require('../data/endpoints.json');
+const authData = require('../data/auth.json');
+const pactum = require('pactum');
+const responseData = require("../data/response.json");
+const requestJSON = require("../data/createBooking.json");
+const request = pactum.request;
+
+request.setBaseUrl('http://localhost:3001');
+
+it('Create token - valid', async () => {
+    await pactum.spec()
+        .post(endpoints.getToken)
+        .withHeaders('Accept', 'application/json')
+        .withJson({
+            username: authData.validUsername,
+            password: authData.validPassword
+        })
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJsonSchema('token', {
+            "type": "string"
+        });
+});
+
+it('Create token - invalid username', async () => {
+    await pactum.spec()
+        .post(endpoints.getToken)
+        .withHeaders('Accept', 'application/json')
+        .withJson({
+            username: authData.invalidUsername,
+            password: authData.validPassword
+        })
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJson('reason', responseData.badCredentialsMessage);
+});
+
+it('Create token - invalid password', async () => {
+    await pactum.spec()
+        .post(endpoints.getToken)
+        .withHeaders('Accept', 'application/json')
+        .withJson({
+            username: authData.validUsername,
+            password: authData.invalidPassword
+        })
+        .expectStatus(200)
+        .expectHeaderContains('content-type', 'application/json')
+        .expectJson('reason', responseData.badCredentialsMessage);
+});
