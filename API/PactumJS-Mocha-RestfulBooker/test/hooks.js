6a0469e4794f4e8ae7782533b8807789968ef447
azeljkovic <aleksandar.zeljkovic91@gmail.com>
2021-11-17 21:23:31 +0100

    added authentication for booking update

--- API/PactumJS-Mocha-RestfulBooker/test/hooks.js
+++ API/PactumJS-Mocha-RestfulBooker/test/hooks.js
@@ -1,24 +1,9 @@
 const { request } = require('pactum');
-const axios = require("axios");
 const baseURL = 'http://localhost:3001';
 
 exports.mochaHooks = {
     async beforeAll(done) {
         request.setBaseUrl(baseURL);
-        axios({
-            method: 'post',
-            url: baseURL + '/auth',
-            data: {
-                username: 'admin',
-                password: 'password123'
-            }
-        })
-            .then((response) => {
-                global.token = response.data.token;
-                console.log(global.token);
-            }, (error) => {
-                console.log(error);
-            });
 
         done();
     }
--- API/PactumJS-Mocha-RestfulBooker/test/updateBooking.js
+++ API/PactumJS-Mocha-RestfulBooker/test/updateBooking.js
@@ -1,15 +1,14 @@
 const endpoints = require('../data/endpoints.json');
 const requestJSON = require('../data/createBooking.json');
+const authData = require('../data/auth.json');
 const pactum = require('pactum');
-const {mochaHooks} = require("./hooks");
 
 
-it.only('Create valid booking', async () => {
-    await new Promise(resolve => setTimeout(resolve, 1000));
+it.only('Update booking', async () => {
     await pactum.spec()
         .put(endpoints.booking + endpoints.updateID)
+        .withAuth(authData.validUsername, authData.validPassword)
         .withHeaders('Accept', 'application/json')
-        .withCookies('token='+ token)
         .withBody(requestJSON)
         .expectStatus(200)
         .expectHeaderContains('content-type', 'application/json')
